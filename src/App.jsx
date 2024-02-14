d52b173bc40aca3891afb5b1afa3fb357f3b3679
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-02-14 12:32:15 +0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedback, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
 
   return (
-    <div>
-      3D Developer Portfolio
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
     </div>
+    </BrowserRouter>
   )
 }
 
