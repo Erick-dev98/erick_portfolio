8e0aed5456eaa4e6652a8a995fb3b7c5967b2ab2
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-02-15 11:40:07 +0300

    Import 3d models from react-three and Import GLTF model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import { canvas } from "@react-three/fiber";
+//This helps us import 3d models from react-three
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import { CanvasLoader } from '../Loader';
 
 const Computers = () => {
+  //Import GLTF model
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
   return (
     <div>Computers</div>
   )
