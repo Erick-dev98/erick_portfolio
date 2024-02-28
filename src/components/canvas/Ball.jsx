0480ef58d2bb49ef961456b83f20624caeb68191
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-02-28 12:57:58 +0300

    first commit

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import {
   Decal, Float, OrbitControls, Preload, useTexture
@@ -7,47 +7,64 @@ import {
 import CanvasLoader from '../Loader';
 
 const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl])
+  const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1}
-    floatIntensity={2}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25}/>
       <directionalLight position={[0, 0, 0.5]}/>
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial 
-        color="#fff8eb"
-        polygonOffset
-        polygonOffsetFactor={-5}
-        flatShading
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
         />
         <Decal 
-        position={[0, 0, 1]}
-        rotation={[ 2 * Math.PI, 0, 6.25]}
-        map={decal}
-        flatShading
+          position={[0, 0, 1]}
+          rotation={[ 2 * Math.PI, 0, 6.25]}
+          map={decal}
+          flatShading
         />
       </mesh>
     </Float>
-  )
+  );
 }
 
 const BallCanvas = ({ icon }) => {
-  return (
-    <Canvas
-    frameloop='demand'
-    dpr={[1,2]}
-    gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon}/>
-      </Suspense>
+  const [canvasHeight, setCanvasHeight] = useState('auto');
 
-      <Preload all/>
-    </Canvas>
-  )
+  useEffect(() => {
+    const handleResize = () => {
+      const windowHeight = window.innerHeight;
+      const canvasHeight = windowHeight <= 500 ? windowHeight / 2 : 'auto';
+      setCanvasHeight(canvasHeight);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="hidden xs:block" style={{ height: canvasHeight }}>
+      <Canvas
+        frameloop='demand'
+        dpr={[1,2]}
+        gl={{ preserveDrawingBuffer: true }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls enableZoom={false} />
+          <Ball imgUrl={icon}/>
+        </Suspense>
+        <Preload all/>
+      </Canvas>
+    </div>
+  );
 }
 
 export default BallCanvas;
